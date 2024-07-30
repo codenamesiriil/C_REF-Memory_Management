5a0f550eb631a9ae12fde9d3d02f828fe3fbd957
codenamesiriil <codenamesiriil@gmail.com>
2024-07-30 19:50:55 +0100

    header fl

--- cns_mem_man.h
+++ cns_mem_man.h
@@ -4,8 +4,10 @@
 # include <stdlib.h>
 # include <stdio.h>
 
+int     cns_memcmp(const void *s1, const void *s2, size_t n);
 void	*cns_memcpy(void *dst, const void *src, size_t n);
 void	*cns_memset(void *s, int c, size_t n);
 void	*cns_memchr(const void *s, int c, size_t n);
+
 
 #endif
