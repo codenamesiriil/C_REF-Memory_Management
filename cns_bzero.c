8a662d710c4b38c56d6c8cc1dc5f881c0180043c
codenamesiriil <codenamesiriil@gmail.com>
2024-07-31 19:28:42 +0100

    bzero update

---
+++ cns_bzero.c
@@ -0,0 +1,6 @@
+#include "cns_mem_man.h"
+
+void	cns_bzero(void *s, size_t n)
+{
+	cns_memset(s, 0, n);
+}
--- cns_mem_man.h
+++ cns_mem_man.h
@@ -8,6 +8,7 @@ int     cns_memcmp(const void *s1, const void *s2, size_t n);
 void	*cns_memcpy(void *dst, const void *src, size_t n);
 void	*cns_memset(void *s, int c, size_t n);
 void	*cns_memchr(const void *s, int c, size_t n);
+void	cns_bzero(void *s, size_t n);
 
 
 #endif
